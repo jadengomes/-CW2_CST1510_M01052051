4104670103441a354c4808d204019d0165d7b139
jadengomes <JG1243@live.mdx.ac.uk>
2025-12-14 20:20:40 +0400

    Upload CW2 files

--- auth.py
+++ auth.py
@@ -23,6 +23,7 @@ def hash_password(plain_text_password: str) -> str:
 def verify_password(plain_text_password: str, hashed_password: str) -> bool:
     return bcrypt.checkpw(
         plain_text_password.encode("utf-8"),
+        
         hashed_password.encode("utf-8")
     )
 
